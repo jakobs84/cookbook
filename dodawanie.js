bc6bb0d4a8c836ed2e27889ce27248aa4146a765
Jakub <jakobs84@o2.pl>
2022-06-28 01:33:17 +0200

    CSS

--- dodawanie.js
+++ dodawanie.js
@@ -1,14 +1,16 @@
 function suma()
  {
+
    var a = document.getElementById("waga").value;
    var checkBox = document.getElementById("misa");
    //   a = parseFloat(a);
    
+
    if (a > 2000){
     if (checkBox.checked == true){
-        alert((a - 825)/3);
+        alert(((((a - 825)/3))| 0)+" g");
     } else {
-        alert((a - 795)/3);
+        alert((((a - 795)/3)| 0)+" g");
     }
    } else {alert("Za mało!!")};
    
